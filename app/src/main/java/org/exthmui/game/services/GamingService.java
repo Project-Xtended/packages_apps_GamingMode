09eb136dff7efc31d1dbdfadbd6254e379cc6c0e
DennySPb <dennyspb@gmail.com>
2021-09-10 13:59:17 +0300

    GamingMode: Option for disable menu overlay [3/3]

--- app/src/main/java/org/exthmui/game/services/GamingService.java
+++ app/src/main/java/org/exthmui/game/services/GamingService.java
@@ -87,6 +87,8 @@ public class GamingService extends Service {
 
     private GamingPhoneStateListener mPhoneStateListener;
 
+    private boolean mMenuOverlay;
+
     private BroadcastReceiver mGamingModeOffReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -180,8 +182,9 @@ public class GamingService extends Service {
             updateConfig();
         }
 
+        mMenuOverlay = getIntSetting(Constants.ConfigKeys.MENU_OVERLAY, Constants.ConfigDefaultValues.MENU_OVERLAY) == 1 ? true : false;
         mOverlayServiceIntent.putExtras(mCurrentConfig);
-        startServiceAsUser(mOverlayServiceIntent, UserHandle.CURRENT);
+        if (mMenuOverlay) startServiceAsUser(mOverlayServiceIntent, UserHandle.CURRENT);
         Settings.System.putInt(getContentResolver(), Settings.System.GAMING_MODE_ACTIVE, 1);
         if (mTelephonyManager != null) {
             mCallStatusIntent.putExtra("state", mTelephonyManager.getCallState());
@@ -250,6 +253,9 @@ public class GamingService extends Service {
 
         // menu opacity
         mCurrentConfig.putInt(Constants.ConfigKeys.MENU_OPACITY, getIntSetting(Constants.ConfigKeys.MENU_OPACITY, Constants.ConfigDefaultValues.MENU_OPACITY));
+
+        // menu opacity
+        mCurrentConfig.putInt(Constants.ConfigKeys.MENU_OVERLAY, getIntSetting(Constants.ConfigKeys.MENU_OVERLAY, Constants.ConfigDefaultValues.MENU_OVERLAY));
 
         Intent intent = new Intent(Constants.Broadcasts.BROADCAST_CONFIG_CHANGED);
         intent.putExtras(mCurrentConfig);
@@ -337,7 +343,7 @@ public class GamingService extends Service {
     public void onDestroy() {
         unregisterReceiver(mGamingModeOffReceiver);
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mCallControlReceiver);
-        stopServiceAsUser(mOverlayServiceIntent, UserHandle.CURRENT);
+        if (mMenuOverlay) stopServiceAsUser(mOverlayServiceIntent, UserHandle.CURRENT);
         mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_NONE);
         setDisableGesture(false);
         setDisableHwKeys(false, true);
